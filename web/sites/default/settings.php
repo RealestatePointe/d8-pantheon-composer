da4bd733cbeee29da983e584a8bb8d99e5919a9b
Tony Manhollan <tony@realestatepointe.com>
2018-05-03 14:18:30 -0400

    Default redirect settings
    Redirect all Pantheon platform domains to their HTTPS equivalents and set up the basic structure for launch-time redirects.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -52,3 +52,57 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
     $settings['trusted_host_patterns'][] = "{$_ENV['PANTHEON_ENVIRONMENT']}-{$_ENV['PANTHEON_SITE_NAME']}.panth.io";
   }
 }
+
+/**
+ * Redirects
+ * Default configuration redirects Pantheon platform domains to their HTTPS equivalents. 
+ * See https://pantheon.io/docs/domains/#platform-domains
+ * To avoid applying unintended redirects to real hostnames at launch, redirects are only applied in the live 
+ * environment to platform domains. And pre-launch platform domains default to 302 status code.
+ * @todo: At launch, it's important to customize the live environment as follows:
+ *  - Set $primary_domain to a real hostname and remove the platform domain condition
+ *  - Set $redirect_code to 301
+ *. - Add conditions on specific paths from the old site (where relevant) and conditionally set the status code
+ */
+if (isset($_ENV['PANTHEON_ENVIRONMENT']) && $_ENV['PANTHEON_ENVIRONMENT'] != 'lando' && php_sapi_name() != 'cli') {
+  $redirect_uri = $_SERVER['REQUEST_URI'];
+  $redirect_code = 302;
+
+  if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
+    //$primary_domain = 'example.com';
+    //$redirect_code = 301
+    // When launching with a real hostname, uncomment the previous two lines, set the hostname, and remove the following condition
+    if (preg_match('/\.pantheonsite\.io$/', $_SERVER['HTTP_HOST'])) {
+      $primary_domain = $_SERVER['HTTP_HOST'];
+    }
+    // Additional conditions for specific paths, e.g. at launch to redirect old URLs to their new locations
+  } else {
+    $primary_domain = $_SERVER['HTTP_HOST'];
+  }
+
+  if (isset($primary_domain)) {
+    if ($_SERVER['HTTP_HOST'] != $primary_domain
+        || $_SERVER['REQUEST_URI'] != $redirect_uri
+        || !isset($_SERVER['HTTP_USER_AGENT_HTTPS'])
+        || $_SERVER['HTTP_USER_AGENT_HTTPS'] != 'ON' ) {
+
+      if (extension_loaded('newrelic')) {
+        newrelic_name_transaction("redirect");
+      }
+
+      switch ($redirect_code) {
+        case 301:
+          header('HTTP/1.0 301 Moved Permanently');
+          break;
+        case 302:
+          header('HTTP/1.0 302 Found');
+          break;
+        case 307:
+          header('HTTP/1.0 307 Temporary Redirect');
+          break;
+      }
+      header('Location: https://'. $primary_domain . $redirect_uri);
+      exit();
+    }
+  }
+}
