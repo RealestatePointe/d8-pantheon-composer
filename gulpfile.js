e2b816b7f918299a59b4d38302e11c007409c5a1
Tony Manhollan <tony@realestatepointe.com>
2019-07-15 18:36:21 -0400

    Update node_modules and default gulp tasks
    * Request latest versions of gulp and gulp-sass
    * Add gulp-resolve-url and gulp-sourcemaps
    * Add ckeditor-codemirror-plugin
    * Overhaul the default gulp tasks for gulp 4
    * Provide a default task and a libraries task to handle the ckeditor plugin
    * Add some boilerplate structure to handle the result of the master theme starterkit output and include some instructions.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,30 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass');
-
 /**
  * Boilerplate
- * Run the starterkit from repmaster or define a custom theme task and
- * uncomment the default task below. The repmaster starterkit script
- * provides a theme task encapsulating its two default steps, compiling
- * sass and copying bootstrap's js libraries.
+ *
+ * @todo: Run the starterkit from repmaster or define a custom theme task and
+ *   add to the default build task below. The repmaster starterkit script
+ *   provides a theme task encapsulating its three default steps, compiling
+ *   sass, copying bootstrap's js libraries, and assembling assets. A variable
+ *   will be added matching the name of the new theme, which can be used to add
+ *   its exported theme property to the build task.
+ *   e.g.:
+ *     var myNewTheme = require('./web/themes/custom/myNewTheme/gulpfile.js');
+ *     var build = gulp.parallel(libraries, myNewTheme.theme);
+ *
+ * @todo: Remove this comment block after customizing.
  */
-gulp.task('default', []);
+var gulp = require('gulp');
 
+function libraries__ckeditor_codemirror_plugin() {
+  return gulp.src(['node_modules/ckeditor-codemirror-plugin/**'], { base: 'node_modules/ckeditor-codemirror-plugin' })
+    .pipe(gulp.dest('web/libraries/ckeditor_codemirror/'));
+}
+var libraries = gulp.parallel(libraries__ckeditor_codemirror_plugin);
+exports.libraries = libraries;
+
+/* *** BP:THEME *** */
+
+var build = gulp.parallel(libraries);
+
+exports.build = build;
+exports.default = build;
